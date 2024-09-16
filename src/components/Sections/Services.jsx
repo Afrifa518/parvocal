71244729909b405af0fc75d3b00d83cd40f2ad9d
Afrifa518 <nanaoyeeee@gmail.com>
2024-09-16 00:42:33 +0000

    first

---
+++ src/components/Sections/Services.jsx
@@ -0,0 +1,232 @@
+import React from "react";
+import styled from "styled-components";
+// Components
+import ClientSlider from "../Elements/ClientSlider";
+import ServiceBox from "../Elements/ServiceBox";
+import FullButton from "../Buttons/FullButton";
+// Assets
+import AddImage1 from "../../assets/img/add/1.jpg";
+import AddImage2 from "../../assets/img/add/2.jpg";
+import AddImage3 from "../../assets/img/add/3.jpg";
+import AddImage4 from "../../assets/img/add/4.jpg";
+
+export default function Services() {
+  return (
+    <Wrapper id="services">
+      <div className="lightBg" style={{ padding: "50px 0" }}>
+        <div className="container">
+          <ClientSlider />
+        </div>
+      </div>
+      <div className="whiteBg" style={{ padding: "60px 0" }}>
+        <div className="container">
+          <HeaderInfo>
+            <h1 className="font40 extraBold">Empowering Brands with Strategic PR Solutions</h1>
+            <p className="font13">
+            At Parvocal, we offer a suite of services designed to help brands strengthen their relationships with their<br /> 
+            audiences and grow their reputation. Whether you're navigating a media crisis, <br />
+            launching a new product, or reimagining your brand’s story, our team is equipped to deliver impactful results.
+            </p>
+          </HeaderInfo>
+          <ServiceBoxRow className="flex">
+            <ServiceBoxWrapper>
+              <ServiceBox
+                icon="monitor"
+                title="Media Relations"
+                subtitle1="Crafting Your Narrative, Amplifying Your Voice."
+                subtitle="We ensure your story is told in the right places at the right time, enhancing credibility and building your brand’s reputation."
+              />
+            </ServiceBoxWrapper>
+            <ServiceBoxWrapper>
+              <ServiceBox
+                icon="roller"
+                title="Crisis Management"
+                subtitle1="Turning Challenges into Opportunities."
+                subtitle="We help you manage and mitigate crises, ensuring your brand’s reputation remains intact while navigating challenging times."
+              />
+            </ServiceBoxWrapper>
+            <ServiceBoxWrapper>
+              <ServiceBox
+                icon="browser"
+                title="Brand Strategy and Positioning"
+                subtitle1="Building a Brand that Resonates."
+                subtitle="We craft and refine your brand’s story, ensuring your message stands out and resonates with your target audience."
+              />
+            </ServiceBoxWrapper>
+            <ServiceBoxWrapper>
+              <ServiceBox icon="printer" title="Digital PR and Social Media Management" 
+              subtitle1="Connecting Your Brand with the Digital World." 
+              subtitle="We grow your brand’s online presence through strategic digital PR and social media engagement, fostering authentic connections." 
+              />
+            </ServiceBoxWrapper>
+          </ServiceBoxRow>
+        </div>
+        <div className="lightBg">
+          <div className="container">
+            <Advertising className="flexSpaceCenter">
+              <AddLeft>
+                <h4 className="font15 semiBold">A few words about parvocal</h4>
+                <h2 className="font40 extraBold">A Study of Creativity</h2>
+                <p className="font12">
+                Our commitment to excellence has earned us the trust of brands across diverse industries,
+                 all looking to build stronger, more authentic connections with their audience.
+                </p>
+                <ButtonsRow className="flexNullCenter" style={{ margin: "30px 0"}}>
+                  <div style={{ width: "190px" }}>
+                    <FullButton title="Get Started" action={() => alert("clicked")} />
+                  </div>
+                  <div style={{ width: "190px", marginLeft: "15px" }}>
+                    <FullButton title="Contact Us" action={() => alert("clicked")} border />
+                  </div>
+                </ButtonsRow>
+              </AddLeft>
+              <AddRight>
+                <AddRightInner>
+                  <div className="flexNullCenter">
+                    <AddImgWrapp1 className="flexCenter">
+                      <img src={AddImage1} alt="office" />
+                    </AddImgWrapp1>
+                    <AddImgWrapp2>
+                      <img src={AddImage2} alt="office" />
+                    </AddImgWrapp2>
+                  </div>
+                  <div className="flexNullCenter">
+                    <AddImgWrapp3>
+                      <img src={AddImage3} alt="office" />
+                    </AddImgWrapp3>
+                    <AddImgWrapp4>
+                      <img src={AddImage4} alt="office" />
+                    </AddImgWrapp4>
+                  </div>
+                </AddRightInner>
+              </AddRight>
+            </Advertising>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  width: 100%;
+`;
+const ServiceBoxRow = styled.div`
+  @media (max-width: 860px) {
+    flex-direction: column;
+  }
+`;
+const ServiceBoxWrapper = styled.div`
+  width: 20%;
+  margin-right: 5%;
+  padding: 80px 0;
+  @media (max-width: 860px) {
+    width: 100%;
+    text-align: center;
+    padding: 40px 0;
+  }
+`;
+const HeaderInfo = styled.div`
+  @media (max-width: 860px) {
+    text-align: center;
+  }
+`;
+const Advertising = styled.div`
+  margin: 80px 0;
+  padding: 100px 0;
+  position: relative;
+  @media (max-width: 1160px) {
+    padding: 100px 0 40px 0;
+  }
+  @media (max-width: 860px) {
+    flex-direction: column;
+    padding: 0 0 30px 0;
+    margin: 80px 0 0px 0;
+  }
+`;
+const ButtonsRow = styled.div`
+  @media (max-width: 860px) {
+    justify-content: space-between;
+  }
+`;
+const AddLeft = styled.div`
+  width: 50%;
+  p {
+    max-width: 475px;
+  }
+  @media (max-width: 860px) {
+    width: 80%;
+    order: 2;
+    text-align: center;
+    h2 {
+      line-height: 3rem;
+      margin: 15px 0;
+    }
+    p {
+      margin: 0 auto;
+    }
+  }
+`;
+const AddRight = styled.div`
+  width: 50%;
+  position: absolute;
+  top: -70px;
+  right: 0;
+  @media (max-width: 860px) {
+    width: 80%;
+    position: relative;
+    order: 1;
+    top: -40px;
+  }
+`;
+const AddRightInner = styled.div`
+  width: 100%;
+`;
+const AddImgWrapp1 = styled.div`
+  width: 48%;
+  margin: 0 6% 10px 6%;
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 1rem;
+    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+  }
+`;
+const AddImgWrapp2 = styled.div`
+  width: 30%;
+  margin: 0 5% 10px 5%;
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 1rem;
+    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+  }
+`;
+const AddImgWrapp3 = styled.div`
+  width: 20%;
+  margin-left: 40%;
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 1rem;
+    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+  }
+`;
+const AddImgWrapp4 = styled.div`
+  width: 30%;
+  margin: 0 5%auto;
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 1rem;
+    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 2px 15px rgba(0, 0, 0, 0.3);
+  }
+`;
